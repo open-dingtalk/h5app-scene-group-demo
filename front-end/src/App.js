7ce5154faec290936802a74566791c17438a25ff
wb-zr624549 <wb-zr624549@alibaba-inc.com>
2021-10-29 11:56:23 +0800

    feat:优化前端页面

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,169 +1,284 @@
-import logo from './logo.svg';
-import './App.css';
-import * as dd from 'dingtalk-jsapi';
-import axios from 'axios';
-import React from 'react';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import * as dd from "dingtalk-jsapi"
+import axios from "axios"
+import React from "react"
+import "./App.css"
+import { Button, message } from "antd"
+import Form from "./components/Form"
+import "antd/dist/antd.min.css"
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       //内网穿透工具介绍:
       // https://developers.dingtalk.com/document/resourcedownload/http-intranet-penetration?pnamespace=app
       domain: "",
-      corpId: '',
-      authCode: '',
-      userId: '',
-      userName: '',
-      groupId: '',
-      title: '故障维修',
-      topCardId: '',
-      imCardId: '',
-      msg: '一号机房发生故障，不能正常工作，请尽快维修处理!!!',
+      corpId: "",
+      authCode: "",
+      userId: "",
+      userName: "",
+      groupId: "",
+      topCardId: "",
+      imCardId: "",
+      showType: 0,
     }
   }
   render() {
-    if(this.state.userId === ''){
-      this.login();
+    if (this.state.userId === "") {
+      this.login()
     }
     return (
-        <div className="App">
-          <h2>故障处理</h2>
-          <p><small>置顶功能pc端无法查看时，请在手机端查看</small></p>
-          <p><button type="button" onClick={() => this.create()}>发生故障自动建群</button></p>
-          <p><button type="button" onClick={() => this.sendIm()}>推送故障消息（IM卡片）</button></p>
-          <p><button type="button" onClick={(e) => this.updateMsg(e,this.state.imCardId)}>更新消息</button></p>
-          <p><button type="button" onClick={() => this.sendMsg()}>故障消息群置顶（置顶卡片）</button></p>
-          <p><button type="button" onClick={(e) => this.updateMsg(e,this.state.topCardId)}>更新置顶消息（置顶卡片）</button></p>
-          <p><button type="button" onClick={() => this.complete()}>故障修复取消置顶</button></p>
-        </div>
-    );
+      <div className="App">
+        <h2>场景群与消息卡片</h2>
+        <p>
+          <small>置顶功能pc端无法查看时，请在手机端查看</small>
+        </p>
+        {this.state.showType !== 0 && (
+          <Form
+            showType={this.state.showType}
+            onSubmit={(data) => this.onSubmit(data)}
+          />
+        )}
+        {!this.state.showType && (
+          <div>
+            <p>
+              <Button
+                type="primary"
+                onClick={() => this.setState({ showType: 1 })}
+              >
+                创建场景建群
+              </Button>
+            </p>
+            <p>
+              <Button
+                type="primary"
+                onClick={() => this.setState({ showType: 2 })}
+              >
+                推送消息卡片到群
+              </Button>
+            </p>
+            <p>
+              <Button
+                type="primary"
+                onClick={() => this.setState({ showType: 3 })}
+              >
+                更新消息卡片到群
+              </Button>
+            </p>
+            <p>
+              <Button
+                type="primary"
+                onClick={() => this.setState({ showType: 4 })}
+              >
+                消息群置顶卡片
+              </Button>
+            </p>
+            <p>
+              <Button
+                type="primary"
+                onClick={(e) => this.setState({ showType: 5 })}
+              >
+                更新消息群置顶卡片
+              </Button>
+            </p>
+            <p>
+              <Button type="primary" onClick={() => this.complete()}>
+                取消置顶卡片
+              </Button>
+            </p>
+          </div>
+        )}
+      </div>
+    )
   }
-  updateMsg(e, cardId){
-    axios.post(this.state.domain + "/sceneGroup/updateMsg?msg=消息更新——故障修复&cardId=" + cardId
-    ).then(res => {
-      if (res && res.data.success) {
-        if(res.data.data){
-          alert("更新成功！")
-        }else{
-          alert("更新失败！")
+  onSubmit(data) {
+    // 1 创建群 2 发送普通卡片 3 编辑普通卡片 4 发送置顶卡片 5 编辑置顶卡片
+    if (this.state.showType === 1) {
+      this.create(data)
+    } else if (this.state.showType === 2) {
+      this.sendIm(data)
+    } else if (this.state.showType === 3) {
+      this.updateMsg(data, this.state.imCardId)
+    } else if (this.state.showType === 4) {
+      this.sendMsg(data)
+    } else {
+      this.updateMsg(data, this.state.topCardId)
+    }
+  }
+  updateMsg(e, cardId) {
+    if (!cardId) {
+      message.error("请先创建卡片消息")
+      return
+    }
+    axios
+      .post(
+        this.state.domain +
+          "/sceneGroup/updateMsg?msg=" +
+          e.msg +
+          "&cardId=" +
+          cardId
+      )
+      .then((res) => {
+        if (res && res.data.success) {
+          message.success("更新成功！")
+          this.setState({ showType: 0 })
+        } else {
+          message.error("更新失败！")
+          alert("updateMsg failed --->" + JSON.stringify(res))
         }
-      } else {
-        alert("updateMsg failed --->" + JSON.stringify(res));
-      }
-    }).catch(error => {
-      alert("updateMsg err, ", JSON.stringify(error))
-    })
+      })
+      .catch((error) => {
+        alert("updateMsg err, ", JSON.stringify(error))
+      })
   }
-  sendIm(){
-    axios.post(this.state.domain + "/sceneGroup/sendIm?groupId=" + this.state.groupId + "&msg=" + this.state.msg
-    ).then(res => {
-      if (res && res.data.success) {
-        if(res.data.data){
-          this.setState({
-            imCardId:res.data.data
-          })
-          alert("发送成功！")
-        }else{
-          alert("发送失败！")
-        }
-      } else {
-        alert("sendIm failed --->" + JSON.stringify(res));
-      }
-    }).catch(error => {
-      alert("sendIm err, ", JSON.stringify(error))
-    })
-  }
-  complete(){
-    axios.post(this.state.domain + "/sceneGroup/complete?groupId=" + this.state.groupId + "&topCardId=" + this.state.topCardId
-    ).then(res => {
-      if (res && res.data.success) {
-        alert("故障修复完成！");
-      } else {
-        alert("complete failed --->" + JSON.stringify(res));
-      }
-    }).catch(error => {
-      alert("complete err, ", JSON.stringify(error))
-    })
-  }
-  sendMsg(){
-    axios.post(this.state.domain + "/sceneGroup/sendMsg?groupId=" + this.state.groupId + "&msg=" + this.state.msg
-    ).then(res => {
-      if (res && res.data.success) {
-        let topCardId = res.data.data;
-        alert('开启置顶消息成功！！！');
-        this.setState({
-          topCardId:topCardId
-        })
-
-      } else {
-        alert("sendMsg failed --->" + JSON.stringify(res));
-      }
-    }).catch(error => {
-      alert("sendMsg err, ", JSON.stringify(error))
-    })
-  }
-  create(){
-    axios.post(this.state.domain + "/sceneGroup/create?userId=" + this.state.userId + "&title=" + this.state.title
-    ).then(res => {
-      if (res && res.data.success) {
-        let groupId = res.data.data;
-        alert('建群成功！！！');
-        this.setState({
-          groupId:groupId
-        })
-      } else {
-        alert("create failed --->" + JSON.stringify(res));
-      }
-    }).catch(error => {
-      alert("create err, ", JSON.stringify(error))
-    })
-  }
-  login(){
-    axios.get(this.state.domain + "/getCorpId")
-        .then(res => {
-          if(res.data){
-            this.loginAction(res.data);
+  sendIm(data) {
+    axios
+      .post(
+        this.state.domain +
+          "/sceneGroup/sendIm?groupId=" +
+          this.state.groupId +
+          "&msg=" +
+          data.msg
+      )
+      .then((res) => {
+        if (res && res.data.success) {
+          if (res.data.data) {
+            this.setState({
+              imCardId: res.data.data,
+              showType: 0,
+            })
+            message.success("发送成功！")
+          } else {
+            message.error("发送失败！")
           }
-        }).catch(error => {
-      alert("corpId err, ", JSON.stringify(error))
-    })
+        } else {
+          alert("sendIm failed --->" + JSON.stringify(res))
+        }
+      })
+      .catch((error) => {
+        alert("sendIm err, ", JSON.stringify(error))
+      })
+  }
+  complete() {
+    axios
+      .post(
+        this.state.domain +
+          "/sceneGroup/complete?groupId=" +
+          this.state.groupId +
+          "&topCardId=" +
+          this.state.topCardId
+      )
+      .then((res) => {
+        if (res && res.data.success) {
+          this.setState({ showType: 0 })
+          message.success("修复完成！")
+        } else {
+          alert("complete failed --->" + JSON.stringify(res))
+        }
+      })
+      .catch((error) => {
+        alert("complete err, ", JSON.stringify(error))
+      })
+  }
+  sendMsg(data) {
+    axios
+      .post(
+        this.state.domain +
+          "/sceneGroup/sendMsg?groupId=" +
+          this.state.groupId +
+          "&msg=" +
+          data.msg
+      )
+      .then((res) => {
+        if (res && res.data.success) {
+          let topCardId = res.data.data
+          message.success("开启置顶消息成功！！！")
+          this.setState({
+            topCardId: topCardId,
+            showType: 0,
+          })
+        } else {
+          alert("sendMsg failed --->" + JSON.stringify(res))
+        }
+      })
+      .catch((error) => {
+        alert("sendMsg err, ", JSON.stringify(error))
+      })
+  }
+  create(data) {
+    axios
+      .post(
+        this.state.domain +
+          "/sceneGroup/create?userId=" +
+          this.state.userId +
+          "&title=" +
+          data.title
+      )
+      .then((res) => {
+        if (res && res.data.success) {
+          let groupId = res.data.data
+          message.success("建群成功！！！")
+          this.setState({
+            groupId: groupId,
+            showType: 0,
+          })
+        } else {
+          alert("create failed --->" + JSON.stringify(res))
+        }
+      })
+      .catch((error) => {
+        alert("create err, ", JSON.stringify(error))
+      })
+  }
+  login() {
+    axios
+      .get(this.state.domain + "/getCorpId")
+      .then((res) => {
+        if (res.data) {
+          this.loginAction(res.data)
+        }
+      })
+      .catch((error) => {
+        alert("corpId err, ", JSON.stringify(error))
+      })
   }
   loginAction(corpId) {
     // alert("corpId: " +  corpId);
-    let _this = this;
+    let _this = this
     dd.runtime.permission.requestAuthCode({
-      corpId: corpId,//企业 corpId
-      onSuccess : function(res) {
+      corpId: corpId, //企业 corpId
+      onSuccess: function (res) {
         // 调用成功时回调
         _this.state.authCode = res.code
-        axios.get(_this.state.domain + "/login?authCode=" + _this.state.authCode
-        ).then(res => {
-          if (res && res.data.success) {
-            let userId = res.data.data.userId;
-            let userName = res.data.data.userName;
-            alert('登录成功，你好' + userName);
-            setTimeout(function () {
-              _this.setState({
-                userId:userId,
-                userName:userName
-              })
-            }, 0)
-          } else {
-            alert("login failed --->" + JSON.stringify(res));
-          }
-        }).catch(error => {
-          alert("httpRequest failed --->" + JSON.stringify(error))
-        })
+        axios
+          .get(_this.state.domain + "/login?authCode=" + _this.state.authCode)
+          .then((res) => {
+            if (res && res.data.success) {
+              let userId = res.data.data.userId
+              let userName = res.data.data.userName
+              message.success("登录成功，你好" + userName)
+              setTimeout(function () {
+                _this.setState({
+                  userId: userId,
+                  userName: userName,
+                })
+              }, 0)
+            } else {
+              alert("login failed --->" + JSON.stringify(res))
+            }
+          })
+          .catch((error) => {
+            alert("httpRequest failed --->" + JSON.stringify(error))
+          })
       },
-      onFail : function(err) {
+      onFail: function (err) {
         // 调用失败时回调
         alert("requestAuthCode failed --->" + JSON.stringify(err))
-      }
-    });
+      },
+    })
   }
-
 }
 
-export default App;
+export default App
---
+++ front-end/src/components/Form.js
@@ -0,0 +1,49 @@
+import react, { useEffect } from "react"
+import { Form, Input, Button } from "antd"
+
+// showType 1 创建群 2 发送普通卡片 3 编辑普通卡片 4 发送置顶卡片 5 编辑置顶卡片
+const Work = (props) => {
+  const [form] = Form.useForm()
+
+  const onSubmit = (data) => {
+    props.onSubmit(data)
+  }
+  return (
+    <div>
+      <Form form={form} onFinish={onSubmit}>
+        {props.showType === 1 && (
+          <Form.Item
+            label="群名称"
+            name="title"
+            rules={[{ required: true, message: "请输入群名称" }]}
+          >
+            <Input placeholder="请输入群名称" />
+          </Form.Item>
+        )}
+        {(props.showType === 2 || props.showType === 3) && (
+          <Form.Item
+            label="卡片消息"
+            name="msg"
+            rules={[{ required: true, message: "请输入卡片消息内容" }]}
+          >
+            <Input placeholder="请输入卡片消息" />
+          </Form.Item>
+        )}
+        {(props.showType === 4 || props.showType === 5) && (
+          <Form.Item
+            label="置顶卡片消息"
+            name="msg"
+            rules={[{ required: true, message: "请输入卡片消息内容" }]}
+          >
+            <Input placeholder="请输入置顶卡片消息" />
+          </Form.Item>
+        )}
+        <Button htmlType="submit" type="primary">
+          提交
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
+export default Work
